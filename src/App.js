3da5832f7d34f5b8d81930cd7246ede4fb85cdc6
Pham Ngoc Tuong <phamngoctuong1805@gmail.com>
2019-04-11 17:56:25 +0700

    Link

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
+import Contact from './components/Contact';
 class App extends Component {
     render() {
         return (
@@ -11,16 +12,20 @@ class App extends Component {
                     <nav className="navbar navbar-inverse">
                         <ul className="nav navbar-nav">
                             <li className="active">
-                                <a>Home</a>
+                                <Link to="/" className="my-link">Home</Link>
                             </li>
                             <li>
-                                <a>About</a>
+                                <Link to="/about" className="my-link">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact" className="my-link">Contact</Link>
                             </li>
                         </ul>
                     </nav>
                     {/*Content*/}
                     <Route path="/" exact component={Home}></Route>
                     <Route path="/about" component={About}></Route>
+                    <Route path="/contact" component={Contact}></Route>
                 </div>
             </Router>
         );
