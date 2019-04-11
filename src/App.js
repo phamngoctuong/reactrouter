9c64aa23a71995f7275af88b1af7d37ade296a5e
Pham Ngoc Tuong <phamngoctuong1805@gmail.com>
2019-04-11 19:44:34 +0700

    Custom Link

--- src/App.js
+++ src/App.js
@@ -3,52 +3,36 @@ import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+var MenuLink = ({ label, to, activeOnlyWhenExact }) => {
+    return (
+        <Route path={to} exact={activeOnlyWhenExact} children={({match}) => {
+            var active = match ? 'active' : '';
+            return (
+                <li className={active}>
+                    <NavLink to={to} className="my-link">{label}</NavLink>
+                </li>
+            )
+        }}></Route>
+    )
+};
 class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div className="App">
-                    {/*Menu*/}
-                    <nav className="navbar navbar-inverse">
-                        <ul className="nav navbar-nav">
-                            <li className="active" 
-                            >
-                                <NavLink 
-                                    to="/" 
-                                    exact
-                                    className="my-link"
-                                    activeStyle={{
-                                        backgroundColor: "#7b7777"
-                                    }}
-                                >Home</NavLink>
-                            </li>
-                            <li>
-                                <NavLink 
-                                    to="/about" 
-                                    className="my-link"
-                                    activeStyle={{
-                                        backgroundColor: "#7b7777"
-                                    }}
-                                >About</NavLink>
-                            </li>
-                            <li>
-                                <NavLink 
-                                    to="/contact" 
-                                    className="my-link"
-                                    activeStyle={{
-                                        backgroundColor: "#7b7777"
-                                    }}
-                                >Contact</NavLink>
-                            </li>
-                        </ul>
-                    </nav>
-                    {/*Content*/}
-                    <Route path="/" exact component={Home}></Route>
-                    <Route path="/about" component={About}></Route>
-                    <Route path="/contact" component={Contact}></Route>
-                </div>
-            </Router>
-        );
-    }
+render() {
+    return (
+        <Router>
+            <div className="App">
+                <nav className="navbar navbar-inverse">
+                    <ul className="nav navbar-nav">
+                        <MenuLink to="/" label="Trang chủ" activeOnlyWhenExact={true}></MenuLink>
+                        <MenuLink to="/about" label="Giới thiệu" activeOnlyWhenExact={false}></MenuLink>
+                        <MenuLink to="/contact" label="Liên hệ" activeOnlyWhenExact={false}></MenuLink>
+                    </ul>
+                </nav>
+                <Route path="/" exact component={Home}></Route>
+                <Route path="/about" component={About}></Route>
+                <Route path="/contact" component={Contact}></Route>
+            </div>
+        </Router>
+    );
+}
 }
 export default App;
